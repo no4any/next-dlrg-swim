6c9a5830ba9096542fd29aeeaa79914548c8c059
no4any <martin@karry.de>
2026-07-24 12:07:46 +0200

    Updated zod errors

--- packages/next/src/model/registration/swimmer/SelfManagedSwimmer.zod.ts
+++ packages/next/src/model/registration/swimmer/SelfManagedSwimmer.zod.ts
@@ -5,7 +5,7 @@ import { MongoObjectId } from "../../MongoObjectId.zod";
 
 export const SelfManagedSwimmer = SwimmerData.extend({
     type: z.literal(SwimmerType.enum.SELF_MANAGED),
-    email: z.email({ error: "Keine valide E-Mail" }),
+    email: z.email({ error: "Keine valide E-Mail" }).max(255, { error: "E-Mail ist zu lang. Maximal 255 Zeichen sind zulässig." }),
     team: MongoObjectId.optional()
 })
 
--- packages/next/src/model/registration/swimmer/SwimmerData.zod.ts
+++ packages/next/src/model/registration/swimmer/SwimmerData.zod.ts
@@ -5,10 +5,10 @@ import { MongoObjectId } from "../../MongoObjectId.zod";
 export const SwimmerData = z.object({
     _id: MongoObjectId.optional(),
     status: RegistrationStatus.optional(),
-    firstName: z.string().min(2, { error: "Der Vorname muss mindestens 2 Buchstaben haben" }),
-    lastName: z.string().min(2, { error: "Der Nachname muss mindestens 2 Buchstaben haben" }),
+    firstName: z.string().min(2, { error: "Der Vorname muss mindestens 2 Buchstaben haben" }).max(255, {error: "Der Vorname darf nicht mehr als 255 Buchstaben haben" }),
+    lastName: z.string().min(2, { error: "Der Nachname muss mindestens 2 Buchstaben haben" }).max(255, {error: "Der Nachname darf nicht mehr als 255 Buchstaben haben" }),
     gender: z.enum(["0", "M", "W"]).optional(),
-    city: z.string().optional(),
+    city: z.string().min(2, { error: "Der Ort muss mindestens 2 Buchstaben haben" }).max(255, {error: "Der Ort darf nicht mehr als 255 Buchstaben haben" }).optional(),
     //    distanceRating: z.boolean().optional(),
     birthday: z.iso.date().optional(),
     breakfast: z.boolean().optional(),
