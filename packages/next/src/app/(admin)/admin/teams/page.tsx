15ad43c67fd64a05030872c2d3abf429a7c28bd9
no4any <martin@karry.de>
2026-07-29 17:44:33 +0200

    Fixxed missing key

--- packages/next/src/app/(admin)/admin/teams/page.tsx
+++ packages/next/src/app/(admin)/admin/teams/page.tsx
@@ -8,7 +8,7 @@ export default async function TeamsPage() {
     return <div>
         <h1>Teams</h1>
         <div>
-            {teams.map((team) => <div className="flex flex-row gap-4">
+            {teams.map((team, index) => <div key={team._id?.toString() ?? "${index}"} className="flex flex-row gap-4">
                 <div className="flex-4">{team.name}</div>
                 <div className="flex-1 text-right">{team.swimmers.length || 0}</div>
             </div>)}
