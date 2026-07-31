8fd7bf24bec07a833dae2fb03850334747ea50f4
no4any <martin@karry.de>
2026-07-31 12:28:46 +0200

    Added 'Startseite' link to footer

--- packages/next/src/app/(public)/Footer.tsx
+++ packages/next/src/app/(public)/Footer.tsx
@@ -5,5 +5,6 @@ export function Footer() {
         <Link prefetch={false} className="shrink text-dlrg-red hover:text-dlrg-red/80 transition-colors duration-300" href="https://giessen.dlrg.de/impressum-und-datenschutz/">Impressum</Link>
         <Link prefetch={false} className="shrink text-dlrg-red hover:text-dlrg-red/80 transition-colors duration-300" href="/datenschutz">Datenschutz</Link>
         <Link prefetch={false} className="shrink text-dlrg-red hover:text-dlrg-red/80 transition-colors duration-300" href="/admin">Verwaltung</Link>
+        <Link prefetch={false} className="shrink text-dlrg-red hover:text-dlrg-red/80 transition-colors duration-300" href="/">Startseite</Link>
     </footer>
 }
