2e178a86d493f2934fc79c6e0d2a30458f11bdff
no4any <martin@karry.de>
2026-08-02 22:00:46 +0200

    Removed Datenschutz page

--- packages/next/src/app/(public)/Footer.tsx
+++ packages/next/src/app/(public)/Footer.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 export async function Footer() {
     return <footer className="flex flex-col-reverse md:flex-row-reverse gap-4">
         <Link prefetch={false} className="shrink text-dlrg-red hover:text-dlrg-red/80 transition-colors duration-300" href="https://giessen.dlrg.de/impressum-und-datenschutz/">Impressum</Link>
-        <Link prefetch={false} className="shrink text-dlrg-red hover:text-dlrg-red/80 transition-colors duration-300" href="/datenschutz">Datenschutz</Link>
         <Link prefetch={false} className="shrink text-dlrg-red hover:text-dlrg-red/80 transition-colors duration-300" href="/admin">Verwaltung</Link>
         <Link prefetch={false} className="shrink text-dlrg-red hover:text-dlrg-red/80 transition-colors duration-300" href="/ergebnisse">Ergebnisse</Link>
         <Link prefetch={false} className="shrink text-dlrg-red hover:text-dlrg-red/80 transition-colors duration-300" href="/">Startseite</Link>
